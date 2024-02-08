2120477894ad9d7e045b4e7f1d6efb361ca8577d
lingj1206 <101073123+lingj1206@users.noreply.github.com>
2024-02-08 13:29:19 +1300

    yes no checker
    from last year

---
+++ yes no checker.py
@@ -0,0 +1,17 @@
+# yes_no checker
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+        else:
+            print("Please answer yes/no")
+
+
+played_before = yes_no("Have you played this game before? (yes/no): ")
+
+
