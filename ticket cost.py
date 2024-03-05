af2632ba568a988eb8a01d8a5891ba6ebe0e1276
lingj1206 <101073123+lingj1206@users.noreply.github.com>
2024-03-05 13:07:34 +1300

    finished setting up pandas

--- ticket cost.py
+++ ticket cost.py
@@ -1,5 +1,11 @@
 import pandas
 
+
+# currency formatting function
+def currency(x):
+    return f"${x:.2f}"
+
+
 all_names = ["a", "b", "c", "d", "e"]
 all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
 surcharge = [0, 0, 0.53, 0.53, 0]
@@ -11,6 +17,7 @@ mini_movie_dict = {
 }
 
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+mini_movie_frame = mini_movie_frame.set_index('Name')
 
 # Calculate the total cost (ticket + surcharge)
 mini_movie_frame['Total'] = mini_movie_frame['Surcharge']\
@@ -24,9 +31,18 @@ mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 total = mini_movie_frame['Total'].sum()
 profit = mini_movie_frame['Profit'].sum()
 
+# Currency Formatting (uses currency function)
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print("           ---- Ticket Data ----")
+print()
+
 # output total ticket sales and profits
 print(mini_movie_frame)
 print()
+print("---- Ticket cost / profit ----")
 
 # output total ticket sales and profit
 print(f"total Ticket Sales: ${total:.2f}")
